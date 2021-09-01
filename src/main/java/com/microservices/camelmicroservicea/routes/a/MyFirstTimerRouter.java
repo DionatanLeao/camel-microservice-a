2db3aa594b1e5e9ccc08903bbe6210dc1310ca5d
DionatanLeao <dionatan.ti@outlook.com>
2021-09-01 15:30:49 -0300

    Processing using Camel Processors in Camel Routes

--- src/main/java/com/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ src/main/java/com/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -2,12 +2,12 @@ package com.microservices.camelmicroservicea.routes.a;
 
 import java.time.LocalDateTime;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import com.microservices.camelmicroservicea.routes.a.GetCurrentTimeBean.SimpleLoggingProcessingComponent;
 
 /***
  * 
@@ -18,6 +18,7 @@ import com.microservices.camelmicroservicea.routes.a.GetCurrentTimeBean.SimpleLo
 @Component
 public class MyFirstTimerRouter extends RouteBuilder {
 	
+
 	@Autowired
 	private GetCurrentTimeBean getCurrentTimeBean;
 	
@@ -45,7 +46,8 @@ public class MyFirstTimerRouter extends RouteBuilder {
 			bean(getCurrentTimeBean).
 			log("${body}"). //Time now is
 			bean(loggingComponent).
-			log("${body}"). //Logger			
+			log("${body}"). //Logger
+			process(new SimpleLoggingProcessor()).
 		to("log:first-timer"); // database
 		
 	}
@@ -57,7 +59,8 @@ class GetCurrentTimeBean {
 	public String getCurrentTimeBean() {
 		return "Time now is " + LocalDateTime.now();
 	}
-	
+
+}
 @Component
 class SimpleLoggingProcessingComponent {
 	
@@ -67,4 +70,14 @@ class SimpleLoggingProcessingComponent {
 		logger.info("SimpleLoggingProcessingComponent {}", message);
 	}
 }
+
+class SimpleLoggingProcessor implements Processor {
+	
+	private Logger logger = org.slf4j.LoggerFactory.getLogger(SimpleLoggingProcessor.class);
+	
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		logger.info("SimpleLoggingProcessor {}", exchange.getMessage().getBody());			
+	}
+	
 }
