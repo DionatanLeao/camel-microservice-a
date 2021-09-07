719470672341e5b39dd569710909ace86bf6e0f2
DionatanLeao <dionatan.ti@outlook.com>
2021-09-07 11:00:18 -0300

    Exploring Aggregation Enterprise Integration Pattern in Camel

--- src/main/java/com/microservices/camelmicroservicea/routes/EpiPatternsRouter.java
+++ src/main/java/com/microservices/camelmicroservicea/routes/EpiPatternsRouter.java
@@ -1,10 +1,15 @@
 package com.microservices.camelmicroservicea.routes;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
+
+import com.microservices.camelmicroservicea.CurrencyExchange;
 
 /***
  * 
@@ -15,8 +20,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class EpiPatternsRouter extends RouteBuilder {
 	
-	@Autowired
-	private SplitterComponent splitter;
+//	@Autowired
+//	private SplitterComponent splitter;
 
 	@Override
 	public void configure() throws Exception {
@@ -31,11 +36,23 @@ public class EpiPatternsRouter extends RouteBuilder {
 //		.to("activemq:split-queue");
 		
 		//Message, Message2, Message3
-		from("file:files/csv")
-		.convertBodyTo(String.class)
-//		.split(body(),",")
-		.split(method(splitter))
-		.to("activemq:split-queue");
+//		from("file:files/csv")
+//		.convertBodyTo(String.class)
+////		.split(body(),",")
+//		.split(method(splitter))
+//		.to("activemq:split-queue");
+		
+		//Aggregate
+		//Messages => Aggregate
+		//to, 3
+		
+		from("file:files/aggregate-json")
+		.unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+		.aggregate(simple("${body.to}"), new ArrayListAggregationStrategy())
+		.completionSize(3)
+//		.completionTimeout(HIGHEST)
+		.to("log:aggregate-json");
+		
 	}
 
 }
@@ -45,4 +62,25 @@ class SplitterComponent {
 	public List<String> splitInput(String body) {
 		return List.of("ABC", "DFG", "GHI");
 	}
+}
+
+@Component
+class ArrayListAggregationStrategy implements AggregationStrategy {
+
+	 @SuppressWarnings("unchecked")
+	public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+	        Object newBody = newExchange.getIn().getBody();
+	        ArrayList<Object> list = null;
+	        if (oldExchange == null) {
+	            list = new ArrayList<Object>();
+	            list.add(newBody);
+	            newExchange.getIn().setBody(list);
+	            return newExchange;
+	        } else {
+	            list = oldExchange.getIn().getBody(ArrayList.class);
+	            list.add(newBody);
+	            return oldExchange;
+	        }
+	    }
+
 }
